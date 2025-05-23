769fe4b4926355aef1d877484ad18251b56d75a1
Hernan dario calvo <hernandariocalvo@gmail.com>
2025-05-23 10:28:43 -0500

    Modificando inputs

--- registro.js
+++ registro.js
@@ -10,20 +10,31 @@ form.addEventListener("submit", (e) => {
   const profesion = document.getElementById("profesion").value;
   const activo = document.querySelector('input[name="activo"]:checked').value;
 
+  if (
+  nombre.trim() !== "" &&
+  apellido.trim() !== "" &&
+  edad.trim() !== "" && Number(edad) > 0 &&
+  salario.trim() !== "" && Number(salario) > 0 &&
+  profesion.trim() !== ""
+) {
+    const datos = {
+      "nombre":nombre,
+      "edad":edad,
+      "salario":salario,
+      "activo":activo,
+      "apellido":apellido,
+      "profesion":profesion,
+    }
+  
+    const datosPrevios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    datosPrevios.push(datos);
+    localStorage.setItem("usuarios", JSON.stringify(datosPrevios));
+  
+    window.location.href = "index.html";
 
-  const datos = {
-    "nombre":nombre,
-    "edad":edad,
-    "salario":salario,
-    "activo":activo,
-    "apellido":apellido,
-    "profesion":profesion,
+  }else{
+    alert("Tienes que rellenar todos los datos")
   }
 
-  const datosPrevios = JSON.parse(localStorage.getItem("usuarios")) || [];
-  datosPrevios.push(datos);
-  localStorage.setItem("usuarios", JSON.stringify(datosPrevios));
-
-  window.location.href = "index.html";
   
 });
